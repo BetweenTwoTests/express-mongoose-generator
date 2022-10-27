da98ca97d65c87873a3cad218e57962ec455b74d
BetweenTwoTests <betweentwotests@gmail.com>
2022-10-27 15:50:54 -0400

    controller path should follow app/ and paginate route fixed to not clash

--- templates/router.js
+++ templates/router.js
@@ -2,7 +2,7 @@ const {controllerName} = require({controllerPath});
 
 app.get('/{modelName}/', {controllerName}.list);
 app.get('/{modelName}/:id', {controllerName}.show);
-app.get('/{modelName}//paginate', {controllerName}.paginate);
+app.get('/{modelName}/p/paginate', {controllerName}.paginate);
 app.post('/{modelName}/', {controllerName}.create);
 app.put('/{modelName}/:id', {controllerName}.update);
 app.delete('/{modelName}/:id', {controllerName}.remove);
